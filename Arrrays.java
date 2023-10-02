058658cf2516a9534de1c550eab4e83cb9c53f58
Ashlok Raj <132163518+ashlok003@users.noreply.github.com>
2023-10-02 20:44:27 +0530

    Add files via upload

---
+++ Arrrays.java
@@ -0,0 +1,51 @@
+// Q1. Count the number of occurrences of a particular element x.
+import java.util.Arrays;
+import java.util.Scanner;
+public class Arrrays {
+//    static int countOccurrences(int[] arr,int x){
+//        int count = 0;
+//        for(int i=0;i<arr.length;i++){
+//            if(arr[i]==x){
+//                count++;
+//            }
+//        }return count;
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter the length of array: ");
+//        int  n = sc.nextInt();
+//        int[] arr = new int[n];
+//        System.out.println("Enter "+n+" elements: ");
+//        for(int i = 0;i<n;i++){
+//            arr[i] = sc.nextInt();
+//        }
+//        System.out.print("Enter x: ");
+//        int x = sc.nextInt();
+//        System.out.println("Count of x: "+countOccurrences(arr,x));
+//    }
+//    Q2. Find last occurrence of element x  in a given array.
+//    static int lastOccurence(int[] arr,int x){
+//        int lastIndex = -1;
+//        for(int i = 0;i<arr.length;i++){
+//            if( arr[i] == x){
+//                lastIndex = i;
+//            }
+//        }return lastIndex;
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter size of given array: ");
+//        int n = sc.nextInt();
+//        int[] arr = new int[n];
+//        System.out.println("Enter "+n+" elements of given array: ");
+//        for(int i = 0 ;i< n;i++){
+//            arr[i] = sc.nextInt();
+//        }
+//        System.out.println("Enter the value of x: ");
+//        int x = sc.nextInt();
+//        System.out.println("Last occurrence of element x is : "+lastOccurence(arr,x));
+//    }
+//    Q3.
+
+}
